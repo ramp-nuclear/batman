6556971b6f6a28626687f23232b9bcf0d87f2a82
Eshed Magali <esheder@gmail.com>
2026-03-12 14:26:03 +0200

    Improved magnus a little

--- tests/test_exp.py
+++ tests/test_exp.py
@@ -5,6 +5,7 @@ import numpy as np
 import pytest
 import scipy.sparse.linalg as spla
 from hypothesis import given, settings
+from hypothesis.extra.numpy import arrays
 from scipy.sparse import csr_matrix
 
 from batman.exponentiators import CF3Magnus2IPFCRAM as Magnus
@@ -63,22 +64,25 @@ def test_cram_48_on_identity_matrix(
     assert np.allclose(cram48_serial(m, z, lambda x: 0.0, v, t), res)
 
 
-non_zero = st.floats(min_value=1e-3, max_value=1.0, allow_infinity=False, allow_nan=False) | st.floats(
-    max_value=-1e-3, min_value=-1.0, allow_infinity=False, allow_nan=False
-)
-vec_strat = st.tuples(non_zero, non_zero)
-mat_strat = st.lists(elements=vec_strat, min_size=2, max_size=2, unique=True)
+def minus_major(m: np.ndarray) -> csr_matrix:
+    m[0, 0] = -abs(m[0, 1]) - abs(m[0, 0])
+    m[1, 1] = -abs(m[1, 0]) - abs(m[1, 1])
+    return csr_matrix(m)
+
+
+positives = st.floats(min_value=0, max_value=10, allow_subnormal=False)
+mvalues = st.floats(min_value=-10, max_value=10, allow_subnormal=False)
+vec_strat = arrays(dtype=float, shape=2, elements=positives)
+mat_strat = arrays(dtype=float, shape=(2, 2), elements=mvalues).map(minus_major)
 time_strat = st.floats(min_value=0.5, max_value=3.0)
 
 
 @settings(max_examples=100)
 @given(m=mat_strat, v=vec_strat, t=time_strat)
-def test_cram48_vs_pade_on_identity_matrix(cram48_serial: Exponentiator, m: csr_matrix, v: np.ndarray, t: float):
-    m = csr_matrix(m)
-    z = csr_matrix(m.shape)
-    vec = np.array(v)
-    cramsol = cram48_serial(m, z, lambda x: 0.0, vec.copy(), t)
-    padesol = spla.expm_multiply(m, vec, 0.0, t, num=5, endpoint=True)[-1]
+def test_cram48_vs_pade_on_2x2(cram48_serial: Exponentiator, m: csr_matrix, v: np.ndarray, t: float):
+    z = csr_matrix(np.zeros((2, 2)))
+    cramsol = cram48_serial(m, z, lambda x: 0.0, v.copy(), t)
+    padesol = spla.expm_multiply(m, v, 0.0, t, num=5, endpoint=True)[-1]
     assert np.allclose(cramsol, padesol, rtol=1e-3)
 
 
@@ -87,8 +91,7 @@ def test_cram48_vs_pade_on_identity_matrix(cram48_serial: Exponentiator, m: csr_
 def test_cram_is_same_as_magnus_for_constant_flux(
     cram48_serial: Exponentiator, magnus48: Exponentiator, d: csr_matrix, r: csr_matrix, n: np.array, t: float
 ):
-    d, r = csr_matrix(d), csr_matrix(r)
-    n = np.array(n)
-    cramsol = cram48_serial(d, r, lambda x: 1.0, n, t)
+    d, r = map(csr_matrix, (d, r))
+    cramsol = cram48_serial(d, r, lambda x: 1.0, n.copy(), t)
     magnussol = magnus48(d, r, lambda x: 1.0, n, t)
     assert np.allclose(cramsol, magnussol)
